3eca8065705fb2a4e1ec8ef2f5e8a5bcc6938084
Abhi9824 <abhijitchanda2000@gmail.com>
2024-11-25 13:14:12 +0530

    fix:footer in Checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -9,6 +9,7 @@ import { deleteAddress, fetchAllAddress } from "../../features/addressSlice";
 import { fetchCartProducts } from "../../features/cartSlice";
 import { addAddress } from "../../features/addressSlice";
 import { toast } from "react-toastify";
+import Footer from "../../components/Footer/Footer";
 
 const Checkout = () => {
   const dispatch = useDispatch();
@@ -80,125 +81,130 @@ const Checkout = () => {
   const grandTotal = subtotal - discount + tax + shipping;
 
   return (
-    <div className="container">
-      <div>
-        <Navbar />
-      </div>
-      <div className="row py-4">
-        <div className="col-md-6">
-          <div className="d-flex justify-content-between">
-            <h5>Select Delivery Address</h5>
-            <button
-              className="addAdrres"
-              onClick={() => {
-                setEditAddress(null);
-                setShowForm(true);
-              }}
-            >
-              ADD NEW ADDRESS
-            </button>
-          </div>
-          <p className="fontSize">DEFAULT ADDRESS</p>
-          <div className="address-container">
-            {addresses?.map((address) => (
-              <div
-                key={address._id}
-                className="address-card card mb-2 py-4 mt-4"
+    <>
+      <div className="container">
+        <div>
+          <Navbar />
+        </div>
+        <div className="row py-4 mb-4">
+          <div className="col-md-6">
+            <div className="d-flex justify-content-between">
+              <h5>Select Delivery Address</h5>
+              <button
+                className="addAdrres"
+                onClick={() => {
+                  setEditAddress(null);
+                  setShowForm(true);
+                }}
               >
-                <div className="card-body d-flex align-items-start">
-                  {/* Added radio button for selecting address */}
-                  <div>
-                    <input
-                      type="radio"
-                      name="selectedAddress"
-                      value={address._id}
-                      checked={selectedAddress === address._id}
-                      onChange={() => setSelectedAddress(address._id)}
-                      className="me-2"
-                    />
-                  </div>
-                  <div className="px-2">
-                    <h6>{address.address?.locality}</h6>
-                    <p>
-                      {address.address?.street}
-                      <br />
-                      {address.address?.city}, {address.address?.state} -{" "}
-                      {address.address?.pinCode}
-                    </p>
-                    <p>
-                      Mobile: <b>{address.contact?.phoneNumber}</b>
-                    </p>
-                    <div className="d-flex">
-                      <button
-                        className="btn edit rounded"
-                        onClick={() => editHandler(userId, address)}
-                      >
-                        Edit
-                      </button>
-                      <button
-                        className="btn remove rounded"
-                        onClick={() => removeHandler(address._id)}
-                      >
-                        Remove
-                      </button>
+                ADD NEW ADDRESS
+              </button>
+            </div>
+            <p className="fontSize">DEFAULT ADDRESS</p>
+            <div className="address-container">
+              {addresses?.map((address) => (
+                <div
+                  key={address._id}
+                  className="address-card card mb-2 py-4 mt-4"
+                >
+                  <div className="card-body d-flex align-items-start">
+                    {/* Added radio button for selecting address */}
+                    <div>
+                      <input
+                        type="radio"
+                        name="selectedAddress"
+                        value={address._id}
+                        checked={selectedAddress === address._id}
+                        onChange={() => setSelectedAddress(address._id)}
+                        className="me-2"
+                      />
+                    </div>
+                    <div className="px-2">
+                      <h6>{address.address?.locality}</h6>
+                      <p>
+                        {address.address?.street}
+                        <br />
+                        {address.address?.city}, {address.address?.state} -{" "}
+                        {address.address?.pinCode}
+                      </p>
+                      <p>
+                        Mobile: <b>{address.contact?.phoneNumber}</b>
+                      </p>
+                      <div className="d-flex">
+                        <button
+                          className="btn edit rounded"
+                          onClick={() => editHandler(userId, address)}
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="btn remove rounded"
+                          onClick={() => removeHandler(address._id)}
+                        >
+                          Remove
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+          </div>
+          <div className="col-md-4 mx-1 py-1 mb-5">
+            <div className="card priceCard px-3 d-flex">
+              <h5 className="fw-bold">ORDER SUMMARY</h5>
+              <hr className="mt-0" />
+              <p>
+                Price({cartProducts.length} Item):{" "}
+                <span className="float-end">${subtotal.toFixed(2)}</span>
+              </p>
+              <p>
+                Discount:{" "}
+                <span className="float-end">${discount.toFixed(2)}</span>
+              </p>
+              <p>
+                Tax: <span className="float-end">${tax.toFixed(2)}</span>
+              </p>
+              <p>
+                Shipping Charges:{" "}
+                <span className="float-end">${shipping.toFixed(2)}</span>
+              </p>
+              <hr />
+              <h5 className="fw-bold">
+                Grand Total:{" "}
+                <span className="float-end">${grandTotal.toFixed(2)}</span>
+              </h5>
+              <hr />
+              <p>You will save ${discount.toFixed(0)} on this order.</p>
+              <Link to={`/checkout`}>
+                <button className="btn placeOrder mt-2 w-100 py-2">
+                  Place Order{" "}
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="col-md-4 mx-1 ">
-          <div className="card priceCard px-3 d-flex">
-            <h5 className="fw-bold">ORDER SUMMARY</h5>
-            <hr className="mt-0" />
-            <p>
-              Price({cartProducts.length} Item):{" "}
-              <span className="float-end">${subtotal.toFixed(2)}</span>
-            </p>
-            <p>
-              Discount:{" "}
-              <span className="float-end">${discount.toFixed(2)}</span>
-            </p>
-            <p>
-              Tax: <span className="float-end">${tax.toFixed(2)}</span>
-            </p>
-            <p>
-              Shipping Charges:{" "}
-              <span className="float-end">${shipping.toFixed(2)}</span>
-            </p>
-            <hr />
-            <h5 className="fw-bold">
-              Grand Total:{" "}
-              <span className="float-end">${grandTotal.toFixed(2)}</span>
-            </h5>
-            <hr />
-            <p>You will save ${discount.toFixed(0)} on this order.</p>
-            <Link to={`/checkout`}>
-              <button className="btn placeOrder mt-2 w-100 py-2">
-                Place Order{" "}
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
 
-      {showForm && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={() => setShowForm(false)}>
-              &times;
-            </span>
-            <AddressForm
-              setShowForm={setShowForm}
-              existingAddress={editAddress}
-              onSubmit={addHandler}
-              userId={user?._id}
-            />
+        {showForm && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={() => setShowForm(false)}>
+                &times;
+              </span>
+              <AddressForm
+                setShowForm={setShowForm}
+                existingAddress={editAddress}
+                onSubmit={addHandler}
+                userId={user?._id}
+              />
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+      <div className="mt-4 pt-4">
+        <Footer />
+      </div>
+    </>
   );
 };
 
